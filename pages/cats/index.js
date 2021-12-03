05ed7bff7563d185af498a91fe6521f684b35f7c
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2021-12-03 11:10:38 +0100

    UseMemo finish

---
+++ pages/cats/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
+const kittySchema = new mongoose.Schema({
+    name: String
+  });
+const Kitten = mongoose.model('Kitten', kittySchema);
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name);
